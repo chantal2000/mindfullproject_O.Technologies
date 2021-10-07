4d7da755547ebf7bd4d0a39cd014928926511ddf
chantal2000 <79141873+chantal2000@users.noreply.github.com>
2021-10-07 09:30:14 +0300

    Update database.sql

--- backend/database.sql
+++ backend/database.sql
@@ -2,5 +2,6 @@ CREATE DATABASE mindfulproject;
 CREATE TABLE mindfull(
     user_id SERIAL PRIMARY KEY,
     article_title VARCHAR(40),
-    article_description VARCHAR(30) 
+    article_description VARCHAR(254) ,
+    song_name VARCHAR(30)
 );
