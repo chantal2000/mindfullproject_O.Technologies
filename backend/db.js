cb3f2e40b4b7238ca24e87ed506ef784e1f7055f
bob <nniyonkuruchantal@gmail.com>
2021-10-09 21:00:49 +0300

    securing databse

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: "mindfull",
-    password: "mindfull",
-    host: "localhost",
-    port: 5432,
-    database: "mindfuldb"
+    user: USER_NAME,
+    password: PASSWORD,
+    host: HOST_NAME,
+    port: PORT_NUMBER,
+    database: DATABASE_NAME
 });
 
 pool.connect(() => {
